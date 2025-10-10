ee31c33838e431cb5ae3292639bfc2a194057477
Diego Jaramillo <df.jaramilloa1@uniandes.edu.co>
2025-10-10 09:17:30 -0500

    cambio en apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com' // Replace with production base_url
+  apiUrl: 'https://medisupply-gateway-gw-d7fde8rj.uc.gateway.dev/'
 };
